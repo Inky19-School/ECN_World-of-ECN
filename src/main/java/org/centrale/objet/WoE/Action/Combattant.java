cc0e3358ff248c807c0ed012a3b3a8075f6ac9ee
Inky19 <f.marie260@gmail.com>
2022-10-04 15:40:07 +0200

    added interface and joueur

---
+++ src/main/java/org/centrale/objet/WoE/Action/Combattant.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package org.centrale.objet.WoE.Action;
+
+import org.centrale.objet.WoE.Creature.Creature;
+
+/**
+ *
+ * @author inky19
+ */
+public interface Combattant {
+    public void combattre(Creature c);
+}
---
+++ src/main/java/org/centrale/objet/WoE/Action/Deplacable.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package org.centrale.objet.WoE.Action;
+
+import org.centrale.objet.WoE.World;
+
+/**
+ *
+ * @author inky19
+ */
+public interface Deplacable {
+
+    public void deplacer(World monde);
+}
