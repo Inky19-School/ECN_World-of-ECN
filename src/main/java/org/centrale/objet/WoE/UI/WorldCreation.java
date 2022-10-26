d42ebcb43cb3d0ca1b9b930c568e86cab9e9cf2d
Rémi <Rmi@130.66.210.128>
2022-10-26 20:25:41 +0200

    commit de merde2

--- src/main/java/org/centrale/objet/WoE/UI/WorldCreation.java
+++ src/main/java/org/centrale/objet/WoE/UI/WorldCreation.java
@@ -13,6 +13,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.Container;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
@@ -44,6 +45,7 @@ public class WorldCreation implements Screen {
     private final SpriteBatch sb;
     private final TextField worldNameField; 
     private final TextButton createButton;
+    private final TextField playerNameField;
 
     public WorldCreation(Boot game) {
         this.game = game;
@@ -61,10 +63,11 @@ public class WorldCreation implements Screen {
         textButtonStyle.font = new BitmapFont();
         textButtonStyle.up = skin2.getDrawable("blue_button");
         textButtonStyle.over = skin2.getDrawable("blue_button_selected");
+        textButtonStyle.disabledFontColor = Color.GRAY;
         SelectBox<String> selectBox = new SelectBox<>(skin, "small");
         worldNameField = new TextField("", skin);
         worldNameField.setMaxLength(MAXLENGTH);
-        TextField playerNameField = new TextField("", skin);
+        playerNameField = new TextField("", skin);
         playerNameField.setMaxLength(MAXLENGTH);
 
         table = new Table();
@@ -88,6 +91,7 @@ public class WorldCreation implements Screen {
         selectBox.setItems(items);
         //Buttons      
         createButton = new TextButton("Create New World", textButtonStyle);
+        
         TextButton backButton = new TextButton("Back", skin);
         createButton.addListener(new ChangeListener() {
             @Override
@@ -114,11 +118,11 @@ public class WorldCreation implements Screen {
         table.add(new Label("World settings", skin, "default-font", Color.WHITE)).colspan(3).pad(40).row();
         table.add(new Label("Name ", skin, "small-font", Color.WHITE)).padBottom(20).left();
         table.add(worldNameField).width(300).height(40).colspan(2).right().padBottom(20).row();
-
+        /*
         table.add(new Label("Size ", skin, "small-font", Color.WHITE)).left();
         table.add(sizeSlider).fill();
         table.add(sizeLabel).width(50).right().row();
-
+        */
         //Player
         table.add(new Label("Player settings", skin)).colspan(3).pad(40).row();
 
@@ -146,10 +150,14 @@ public class WorldCreation implements Screen {
         sb.draw(MainMenu.background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
         sb.end();
         // tell our stage to do actions and draw itself
-        if (worldNameField.getText().equals("")) {
+        if (worldNameField.getText().equals("") || playerNameField.getText().equals("")) {
             createButton.setDisabled(true);
+            createButton.setTouchable(Touchable.disabled);
+            createButton.setColor(Color.GRAY);
         } else {
             createButton.setDisabled(false);
+            createButton.setTouchable(Touchable.enabled);
+            createButton.setColor(Color.WHITE);
         }
         
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
